40cf9177b69f0ce1aa38b305faece94b632200cb
David Krutsko <dave@krutsko.net>
2015-08-03 14:42:27 -0400

    Implemented Process Declarations

--- Native/Addon/Process.h
+++ Native/Addon/Process.h
@@ -11,4 +11,55 @@
 // Prefaces                                                                   //
 //----------------------------------------------------------------------------//
 
+#ifndef ADDON_PROCESS_H
+#define ADDON_PROCESS_H
 
+#include "Common.h"
+
+
+
+//----------------------------------------------------------------------------//
+// Classes                                                                    //
+//----------------------------------------------------------------------------//
+
+////////////////////////////////////////////////////////////////////////////////
+
+class ProcessWrap : public ObjectWrap
+{
+	friend class WindowWrap;
+
+private:
+	 ProcessWrap					(void);
+	~ProcessWrap					(void);
+
+private:
+	static void		 Open			(const FunctionCallbackInfo<Value>& args);
+	static void		Close			(const FunctionCallbackInfo<Value>& args);
+
+	static void		IsValid			(const FunctionCallbackInfo<Value>& args);
+	static void		IsDebugged		(const FunctionCallbackInfo<Value>& args);
+
+	static void		Exit			(const FunctionCallbackInfo<Value>& args);
+	static void		Kill			(const FunctionCallbackInfo<Value>& args);
+	static void		HasExited		(const FunctionCallbackInfo<Value>& args);
+
+	static void		GetModules		(const FunctionCallbackInfo<Value>& args);
+	static void		GetWindows		(const FunctionCallbackInfo<Value>& args);
+
+	static void		GetList			(const FunctionCallbackInfo<Value>& args);
+	static void		GetCurrent		(const FunctionCallbackInfo<Value>& args);
+	static void		IsSys64Bit		(const FunctionCallbackInfo<Value>& args);
+
+	static void		New				(const FunctionCallbackInfo<Value>& args);
+
+public:
+	static void		Initialize		(Handle<Object> exports);
+
+private:
+	Process mProcess;
+
+	// Function which defines class creator
+	static Persistent<Function> constructor;
+};
+
+#endif // ADDON_PROCESS_H
