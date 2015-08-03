f7e8c614664b77dfda46772149b70eaa0e570a42
David Krutsko <dave@krutsko.net>
2015-08-03 14:43:00 -0400

    Implemented Process Functions

--- Library/Process.js
+++ Library/Process.js
@@ -16,6 +16,16 @@
 module.exports = function (robot, native)
 {
 	//----------------------------------------------------------------------------//
+	// Locals                                                                     //
+	//----------------------------------------------------------------------------//
+
+	////////////////////////////////////////////////////////////////////////////////
+
+	var mIs64 = null;
+
+
+
+	//----------------------------------------------------------------------------//
 	// Constructor                                                        Process //
 	//----------------------------------------------------------------------------//
 
@@ -23,6 +33,24 @@ module.exports = function (robot, native)
 
 	function Process (pid)
 	{
+		// Auto instantiate the Process
+		if (!(this instanceof Process))
+			return new Process (pid);
+
+		this._procID  = 0;
+		this._name    = "";
+		this._path    = "";
+		this._is64Bit = false;
+		this._process = null;
+
+		if (pid !== null)
+		{
+			// Current constructor is unmanaged
+			this._process = new native.Process();
+
+			// Check whether open should be called
+			if (pid !== undefined) this.open (pid);
+		}
 	}
 
 
@@ -35,84 +63,120 @@ module.exports = function (robot, native)
 
 	Process.prototype.open = function (pid)
 	{
+		// Verify that pid is valid
+		if (typeof pid !== "number")
+			throw new TypeError ("Invalid arguments");
+
+		var result =
+			// Perform a native open
+			this._process.open (pid);
+
+		// Update cached process data
+		this._procID  = result.procID;
+		this._name    = result.name;
+		this._path    = result.path;
+		this._is64Bit = result.is64Bit;
+
+		// Return the result
+		return result.status;
 	};
 
 	////////////////////////////////////////////////////////////////////////////////
 
 	Process.prototype.close = function()
 	{
+		// Close the process
+		this._process.close();
+
+		// Reset cached data
+		this._procID  = 0;
+		this._name    = "";
+		this._path    = "";
+		this._is64Bit = false;
 	};
 
 	////////////////////////////////////////////////////////////////////////////////
 
 	Process.prototype.isValid = function()
 	{
+		return this._process.isValid();
 	};
 
 	////////////////////////////////////////////////////////////////////////////////
 
 	Process.prototype.is64Bit = function()
 	{
+		return this._is64Bit;
 	};
 
 	////////////////////////////////////////////////////////////////////////////////
 
 	Process.prototype.isDebugged = function()
 	{
+		return this._process.isDebugged();
 	};
 
 	////////////////////////////////////////////////////////////////////////////////
 
 	Process.prototype.getPID = function()
 	{
+		return this._procID;
 	};
 
 	////////////////////////////////////////////////////////////////////////////////
 
 	Process.prototype.getMemory = function()
 	{
+		// NYI: Not Yet Implemented
 	};
 
 	////////////////////////////////////////////////////////////////////////////////
 
 	Process.prototype.getName = function()
 	{
+		return this._name;
 	};
 
 	////////////////////////////////////////////////////////////////////////////////
 
 	Process.prototype.getPath = function()
 	{
+		return this._path;
 	};
 
 	////////////////////////////////////////////////////////////////////////////////
 
 	Process.prototype.exit = function()
 	{
+		this._process.exit();
 	};
 
 	////////////////////////////////////////////////////////////////////////////////
 
 	Process.prototype.kill = function()
 	{
+		this._process.kill();
 	};
 
 	////////////////////////////////////////////////////////////////////////////////
 
 	Process.prototype.hasExited = function()
 	{
+		return this._process.hasExited();
 	};
 
 	////////////////////////////////////////////////////////////////////////////////
 
 	Process.prototype.getModules = function (name)
 	{
+		// NYI: Not Yet Implemented
 	};
 
 	////////////////////////////////////////////////////////////////////////////////
 
 	Process.prototype.getWindows = function (title)
 	{
+		// NYI: Not Yet Implemented
 	};
 
 
@@ -125,18 +189,60 @@ module.exports = function (robot, native)
 
 	Process.getList = function (name)
 	{
+		// Verify that name is valid
+		if (name !== undefined &&
+			typeof name !== "string")
+			throw new TypeError ("Invalid arguments");
+
+		// Retrieve the list of all system processes
+		var results = native.Process.getList (name);
+
+		var list = new Array (results.length);
+		// Iterate through the list of processes
+		for (var i = 0; i < results.length; ++i)
+		{
+			// Create new managed process
+			var process = Process (null);
+
+			// Set the cached process data
+			process._procID  = results[i].procID;
+			process._name    = results[i].name;
+			process._path    = results[i].path;
+			process._is64Bit = results[i].is64Bit;
+			process._process = results[i].process;
+			list[i] = process;
+		}
+
+		return list;
 	};
 
 	////////////////////////////////////////////////////////////////////////////////
 
 	Process.getCurrent = function()
 	{
+		var result =
+			// Get the current process
+			native.Process.getCurrent();
+
+		// Create new managed process
+		var process = Process (null);
+
+		// Set the cached process data
+		process._procID  = result.procID;
+		process._name    = result.name;
+		process._path    = result.path;
+		process._is64Bit = result.is64Bit;
+		process._process = result.process;
+		return process;
 	};
 
 	////////////////////////////////////////////////////////////////////////////////
 
 	Process.isSys64Bit = function()
 	{
+		return (mIs64 !== null) ? mIs64 :
+			// Set value once then cache result
+			(mIs64 = native.Process.isSys64Bit());
 	};
 
 
@@ -149,12 +255,30 @@ module.exports = function (robot, native)
 
 	Process.prototype.eq = function (value)
 	{
+		// Check for another process
+		if (value instanceof Process)
+			return this._procID === value._procID;
+
+		// Check process ID comparison
+		if (typeof value === "number")
+			return this._procID === value;
+
+		throw new TypeError ("Invalid arguments");
 	};
 
 	////////////////////////////////////////////////////////////////////////////////
 
 	Process.prototype.ne = function (value)
 	{
+		// Check for another process
+		if (value instanceof Process)
+			return this._procID !== value._procID;
+
+		// Check process ID comparison
+		if (typeof value === "number")
+			return this._procID !== value;
+
+		throw new TypeError ("Invalid arguments");
 	};
 
 	////////////////////////////////////////////////////////////////////////////////
--- Native/Addon/Process.cc
+++ Native/Addon/Process.cc
@@ -25,82 +25,222 @@ SET_WRAP (Process);
 
 void ProcessWrap::Open (const FunctionCallbackInfo<Value>& args)
 {
+	ISOLATE; UNWRAP (Process, args.Holder());
+
+	// Try and open the process
+	bool status = mProcess->Open
+		(args[0]->Int32Value());
+
+	auto res = NEW_OBJ;
+	res->Set (NEW_STR ("procID" ), NEW_INT  (mProcess->GetPID ()));
+	res->Set (NEW_STR ("is64Bit"), NEW_BOOL (mProcess->Is64Bit()));
+	res->Set (NEW_STR ("name"   ), NEW_STR  (mProcess->GetName().data()));
+	res->Set (NEW_STR ("path"   ), NEW_STR  (mProcess->GetPath().data()));
+	res->Set (NEW_STR ("status" ), NEW_BOOL (status));
+	RETURN (res);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 
 void ProcessWrap::Close (const FunctionCallbackInfo<Value>& args)
 {
+	FAST_VOID (Process, args.Holder(), Close());
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 
 void ProcessWrap::IsValid (const FunctionCallbackInfo<Value>& args)
 {
+	FAST_BOOL (Process, args.Holder(), IsValid());
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 
 void ProcessWrap::IsDebugged (const FunctionCallbackInfo<Value>& args)
 {
+	FAST_BOOL (Process, args.Holder(), IsDebugged());
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 
 void ProcessWrap::Exit (const FunctionCallbackInfo<Value>& args)
 {
+	FAST_VOID (Process, args.Holder(), Exit());
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 
 void ProcessWrap::Kill (const FunctionCallbackInfo<Value>& args)
 {
+	FAST_VOID (Process, args.Holder(), Kill());
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 
 void ProcessWrap::HasExited (const FunctionCallbackInfo<Value>& args)
 {
+	FAST_BOOL (Process, args.Holder(), HasExited());
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 
 void ProcessWrap::GetModules (const FunctionCallbackInfo<Value>& args)
 {
+	// NYI: Not Yet Implemented
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 
 void ProcessWrap::GetWindows (const FunctionCallbackInfo<Value>& args)
 {
+	ISOLATE;
+	UNWRAP (Process, args.Holder());
+	auto ctor = NEW_CTOR (Window);
+
+	const char* regex = 0;
+	String::Utf8Value value (args[0]);
+	// Retrieve regex value
+	if (args[0]->IsString())
+		regex = *value ? *value : "";
+
+	// Retrieve a list of all process windows
+	auto list = mProcess->GetWindows (regex);
+
+	int length = (int) list.size();
+	auto res = NEW_ARR (length);
+	// Loop array and add to result
+	for (int i = 0; i < length; ++i)
+	{
+		// Create a new instance of wrapper
+		auto instance = ctor->NewInstance();
+		UNWRAP (Window, instance);
+
+		// Make wrapper use new window
+		mWindowWrap->mWindow = list[i];
+
+		auto obj = NEW_OBJ;
+		auto handle = NEW_NUM ((double) list[i].GetHandle());
+		obj->Set (NEW_STR ("handle"), handle  );
+		obj->Set (NEW_STR ("window"), instance);
+		res->Set (i, obj);
+	}
+
+	RETURN (res);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 
 void ProcessWrap::GetList (const FunctionCallbackInfo<Value>& args)
 {
+	ISOLATE;
+	auto ctor = NEW_CTOR (Process);
+
+	const char* regex = 0;
+	String::Utf8Value value (args[0]);
+	// Retrieve regex value
+	if (args[0]->IsString())
+		regex = *value ? *value : "";
+
+	// Retrieve a list of all processes
+	auto list = Process::GetList (regex);
+
+	int length = (int) list.size();
+	auto res = NEW_ARR (length);
+	// Loop array and add to result
+	for (int i = 0; i < length; ++i)
+	{
+		// Create a new instance of wrapper
+		auto instance = ctor->NewInstance();
+		UNWRAP (Process, instance);
+
+		// Make wrapper use new process
+		mProcessWrap->mProcess = list[i];
+
+		auto obj = NEW_OBJ;
+		obj->Set (NEW_STR ("procID" ), NEW_INT  (list[i].GetPID ()));
+		obj->Set (NEW_STR ("is64Bit"), NEW_BOOL (list[i].Is64Bit()));
+		obj->Set (NEW_STR ("name"   ), NEW_STR  (list[i].GetName().data()));
+		obj->Set (NEW_STR ("path"   ), NEW_STR  (list[i].GetPath().data()));
+		obj->Set (NEW_STR ("process"), instance);
+		res->Set (i, obj);
+	}
+
+	RETURN (res);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 
 void ProcessWrap::GetCurrent (const FunctionCallbackInfo<Value>& args)
 {
+	ISOLATE;
+	auto ctor = NEW_CTOR (Process);
+
+	// Create a new instance of wrapper
+	auto instance = ctor->NewInstance();
+	UNWRAP (Process, instance);
+
+	auto process = Process::GetCurrent();
+	// Make wrapper use new process
+	mProcessWrap->mProcess = process;
+
+	auto res = NEW_OBJ;
+	res->Set (NEW_STR ("procID" ), NEW_INT  (process.GetPID ()));
+	res->Set (NEW_STR ("is64Bit"), NEW_BOOL (process.Is64Bit()));
+	res->Set (NEW_STR ("name"   ), NEW_STR  (process.GetName().data()));
+	res->Set (NEW_STR ("path"   ), NEW_STR  (process.GetPath().data()));
+	res->Set (NEW_STR ("process"), instance);
+	RETURN (res);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 
 void ProcessWrap::IsSys64Bit (const FunctionCallbackInfo<Value>& args)
 {
+	ISOLATE;
+	RETURN_BOOL (Process::IsSys64Bit());
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 
 void ProcessWrap::New (const FunctionCallbackInfo<Value>& args)
 {
+	ISOLATE;
+	// Create new class instance and wrap it
+	(new ProcessWrap())->Wrap (args.This());
+	args.GetReturnValue().Set (args.This());
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 
 void ProcessWrap::Initialize (Handle<Object> exports)
 {
+	// Get the current isolated V8 instance
+	Isolate* isolate = Isolate::GetCurrent();
+
+	// Associate the new function with new template
+	auto tpl = FunctionTemplate::New (isolate, New);
+	tpl->InstanceTemplate()->SetInternalFieldCount (1);
+	tpl->SetClassName (NEW_STR ("Process"));
+
+	NODE_SET_PROTOTYPE_METHOD (tpl, "open",       Open      );
+	NODE_SET_PROTOTYPE_METHOD (tpl, "close",      Close     );
+
+	NODE_SET_PROTOTYPE_METHOD (tpl, "isValid",    IsValid   );
+	NODE_SET_PROTOTYPE_METHOD (tpl, "isDebugged", IsDebugged);
+
+	NODE_SET_PROTOTYPE_METHOD (tpl, "exit",       Exit      );
+	NODE_SET_PROTOTYPE_METHOD (tpl, "kill",       Kill      );
+	NODE_SET_PROTOTYPE_METHOD (tpl, "hasExited",  HasExited );
+
+	NODE_SET_PROTOTYPE_METHOD (tpl, "getModules", GetModules);
+	NODE_SET_PROTOTYPE_METHOD (tpl, "getWindows", GetWindows);
+
+	NODE_SET_METHOD (tpl, "getList",    GetList   );
+	NODE_SET_METHOD (tpl, "getCurrent", GetCurrent);
+	NODE_SET_METHOD (tpl, "isSys64Bit", IsSys64Bit);
+
+	// Assign function template to our class creator
+	constructor.Reset (isolate, tpl->GetFunction());
+	exports->Set
+		  (NEW_STR ("Process"), tpl->GetFunction());
 }
